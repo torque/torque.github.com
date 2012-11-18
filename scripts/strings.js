488fd06d622224506b2111ecd5e4c05ad3a04747
torque <torque@1>
2012-11-18 02:31:13 -0800

    Swich to precompiled CSS.

--- scripts/strings.js
+++
@@ -1,6 +0,0 @@
-var repl, strings;
-strings = ["One step at a time."];
-
-repl = function() {
-  document.getElementById("small").innerHTML = strings[Math.floor(Math.random() * (strings.length - 1))];
-};
